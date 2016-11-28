2bceb4013d639793ab77c1da7f5b650407f2d202
Brian Lee <brianjleeofcl@gmail.com>
2016-11-27 20:01:39 -0800

    Add string validation, lint

--- js/app.js
+++ js/app.js
@@ -3,18 +3,27 @@
 
   const $screen = $('#screen');
 
+  const reduceArray = function(arr, num, prod) {
+    let newArr = arr.slice(0, num - 1);
+
+    newArr = newArr.concat(prod.toPrecision(12));
+    newArr = newArr.concat(...arr.slice(num + 2, arr.length));
+
+    return newArr;
+  };
+
   const multiply = function(arr) {
     const num = arr.indexOf('x');
     const prod = arr[num - 1] * arr[num + 1];
 
-    return arr.slice(0, num - 1).concat(prod.toPrecision(12).toString(), ...arr.slice(num + 2, arr.length));
+    return reduceArray(arr, num, prod);
   };
 
   const divide = function(arr) {
     const num = arr.indexOf('÷');
-    const prod = (arr[num - 1] / arr[num + 1]).toPrecision(12);
+    const prod = arr[num - 1] / arr[num + 1];
 
-    return arr.slice(0, num - 1).concat(prod.toString(), ...arr.slice(num + 2, arr.length));
+    return reduceArray(arr, num, prod);
   };
 
   const multDiv = function(arr) {
@@ -30,12 +39,13 @@
       } else {
         newArr = divide(arr);
       }
-    } else {
-      if (arr.indexOf('x') > 0) {
-        newArr = multiply(arr);
-      } else {
-        newArr = divide(arr);
-      }
+    }
+    else if (arr.indexOf('x') > 0) {
+      newArr = multiply(arr);
+    }
+    else {
+      newArr = divide(arr);
+    }
 
     return multDiv(newArr);
   };
@@ -59,9 +69,24 @@
     return num;
   };
 
+  const validate = function(string) {
+    string = string.replace('/', '÷');
+    string = string.replace(/(\S)(\()/g, '$1 $2');
+    string = string.replace(/(\))(\S)/g, '$1 $2');
+    string = string.replace(/(\()(\s+)/g, '$1');
+    string = string.replace(/(\s+)(\))/g, '$2');
+    string = string.replace(/(\S)([+\-x÷])/g, '$1 $2');
+    string = string.replace(/([+\-x÷])(\S)/g, '$1 $2');
+    string = string.replace(/(\)|\d)(\s)(\(|\d)/g, '$1 x $3');
+
+    return string;
+  };
+
   const calculate = function(string) {
+    string = validate(string);
+
     while (string.includes('(')) {
-      const pths = string.match(/\(.[^\(\)]+\)/);
+      const pths = string.match(/\([^\(\)]+\)/);
       const inside = pths[0].slice(1, -1);
       const calcRes = arithmetic(inside);
 
@@ -116,19 +141,9 @@
         return;
       }
     }
-    if ($(event.target).hasClass('operator')) {
-      $screen.val($screen.val() + ' ');
-    }
-    if (['+', '-', 'x', '÷'].includes($screen.val()[$screen.val().length - 1])) {
-      $screen.val($screen.val() + ' ');
-    }
     if ($screen.val().endsWith('±')) {
       $screen.val($screen.val().replace('±', '-'));
     }
     $screen.val($screen.val() + $(event.target).text());
   });
-
-  // const validate = function (string) {
-  //
-  // }
 })();
